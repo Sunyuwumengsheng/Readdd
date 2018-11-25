66c9129fd075c4340e9d17469410a007bc5757eb
Sunyuwumengsheng <1173353935@qq.com>
2018-11-25 20:35:23 +0800

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/read/MyReadActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.read;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyReadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_read);
+    }
+}
